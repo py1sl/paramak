ef7f73967ceccd20734e692d22ddc01841e7756e
shimwell <mail@jshimwell.com>
2024-11-03 23:26:36 +0100

    producing animation in python

--- examples/spherical_tokamak_from_plasma_with_color_animation.py
+++ examples/spherical_tokamak_from_plasma_with_color_animation.py
@@ -1,4 +1,5 @@
 import paramak
+import imageio
 import cadquery_png_plugin.plugin
 
 # Original radial build values
@@ -14,13 +15,14 @@ original_radial_build = [
     (paramak.LayerType.SOLID, 10),
 ]
 original_elongation = 2
+original_triangularity = 0.55
 
 # Function to create a reactor with modified radial build
-def create_reactor(radial_build, elongation):
+def create_reactor(radial_build, elongation, triangularity):
     return paramak.spherical_tokamak_from_plasma(
         radial_build=radial_build,
         elongation=elongation,
-        triangularity=0.55,
+        triangularity=triangularity,
         rotation_angle=180,
         colors={
             "layer_1": (0.4, 0.9, 0.4),
@@ -45,17 +47,34 @@ def export_reactor_to_png(reactor, file_path):
 
 # Generate reactors with varying radial build values
 frame = 0
+factors = [1.0, 1.25, 1.5, 1.75, 2.0, 1.75, 1.5, 1.25, 1.0]
 for i in range(len(original_radial_build)):
     layer_type, original_value = original_radial_build[i]
-    for factor in [1.0, 1.5, 2.0, 1.5, 1.0]:
+    for factor in factors:
         modified_radial_build = original_radial_build.copy()
         modified_radial_build[i] = (layer_type, original_value * factor)
-        reactor = create_reactor(modified_radial_build, original_elongation)
+        reactor = create_reactor(modified_radial_build, original_elongation, original_triangularity)
         export_reactor_to_png(reactor, f'spherical_tokamak_frame_{frame:03d}.png')
         frame += 1
 
-for factor in [1.0, 1.5, 2.0, 1.5, 1.0]:
+for factor in factors:
     modified_elongation = original_elongation * factor
-    reactor = create_reactor(original_radial_build, modified_elongation)
+    reactor = create_reactor(original_radial_build, modified_elongation, original_triangularity)
     export_reactor_to_png(reactor, f'spherical_tokamak_frame_{frame:03d}.png')
     frame += 1
+
+for modified_triangularity in [0.55, 0.3667, 0.1833, 0.0, -0.1833, -0.3667, -0.55, -0.3667, -0.1833, 0.0, 0.1833, 0.3667, 0.55]:
+    reactor = create_reactor(original_radial_build, original_elongation, modified_triangularity)
+    export_reactor_to_png(reactor, f'spherical_tokamak_frame_{frame:03d}.png')
+    frame += 1
+
+# Convert all the frames PNG files into a GIF
+def create_gif_from_frames(frame_count, output_file):
+    with imageio.get_writer(output_file, mode='I', duration=0.1) as writer:
+        for frame in range(frame_count):
+            filename = f'spherical_tokamak_frame_{frame:03d}.png'
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+# Create the GIF
+create_gif_from_frames(frame, 'spherical_tokamak_animation.gif')
