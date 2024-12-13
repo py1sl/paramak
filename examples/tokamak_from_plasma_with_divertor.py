51518d49d153e27e40d361876eb183d38ab20316
shimwell <mail@jshimwell.com>
2024-12-13 19:12:43 +0100

    moved transport examples to single reactor script

--- examples/tokamak_from_plasma_with_divertor.py
+++ examples/tokamak_from_plasma_with_divertor.py
@@ -31,19 +31,4 @@ my_reactor = paramak.tokamak_from_plasma(
 )
 my_reactor.save(f"tokamak_with_divertor.step")
 print(f"Saved as tokamak_with_divertor.step")
-# vis.show(my_reactor)
 
-# from cad_to_dagmc import CadToDagmc
-# my_model = CadToDagmc()
-# material_tags = ["mat1"] * 6  # as inner and outer layers are one solid there are only 6 solids in model
-# my_model.add_cadquery_object(cadquery_object=my_reactor, material_tags=material_tags)
-# my_model.export_dagmc_h5m_file(min_mesh_size=3.0, max_mesh_size=20.0)
-
-# h5m_filename = "dagmc.h5m"
-# flux = transport_particles_on_h5m_geometry(
-#     h5m_filename=h5m_filename,
-#     material_tags=material_tags,
-#     nuclides=["H1"] * len(material_tags),
-#     cross_sections_xml="tests/cross_sections.xml",
-# )
-# assert flux > 0.0
