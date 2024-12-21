aaa21a137a763e4a605cf784b63a8e4cdce9b871
shimwell <mail@jshimwell.com>
2024-12-21 22:33:14 +0100

    added bounding wedge

--- examples/tokamak_structured_mesh_simulation.py
+++ examples/tokamak_structured_mesh_simulation.py
@@ -1,5 +1,6 @@
 import openmc
-
+import numpy as np
+import math
 import paramak
 
 openmc.config['cross_sections'] = '/nuclear_data/cross_sections.xml' # change this to the dir of your cross_sections.xml
@@ -25,6 +26,7 @@ my_reactor = paramak.tokamak_from_plasma(
 )
 my_reactor.save(f"tokamak_minimal.step")
 print(f"Saved as tokamak_minimal.step")
+my_reactor=my_reactor.remove(name='plasma')
 
 # this prints all the names of the parts in the reactor
 for part in my_reactor:
@@ -32,13 +34,16 @@ for part in my_reactor:
 
 from cad_to_dagmc import CadToDagmc
 my_model = CadToDagmc()
-material_tags = ["layer_1", "layer_2", "layer_3", "layer_4", "layer_5", "plasma"]
+material_tags = ["layer_1", "layer_2", "layer_3", "layer_4", "layer_5"]
 
 # 6 material tags as inner and outer layers are one solid there are only 6 solids in model
 my_model.add_cadquery_object(cadquery_object=my_reactor, material_tags=material_tags)
 
-h5m_filename = "dagmc.h5m"
-# my_model.export_dagmc_h5m_file(filename=h5m_filename, min_mesh_size=10.0, max_mesh_size=20.0)
+from pathlib import Path
+script_path = Path(__file__).resolve()
+script_folder = script_path.parent
+h5m_filename =  script_folder/"dagmc.h5m"
+my_model.export_dagmc_h5m_file(filename=h5m_filename, min_mesh_size=10.0, max_mesh_size=20.0)
 
 # simplified material definitions have been used to keen this example minimal
 mat_layer_1 = openmc.Material(name='layer_1')
@@ -61,18 +66,80 @@ mat_layer_5 = openmc.Material(name='layer_5')
 mat_layer_5.add_nuclide('Fe56', 1, "ao")
 mat_layer_5.set_density("g/cm3", 7)
 
-mat_plasma = openmc.Material(name='plasma')
-mat_plasma.add_nuclide('Fe56', 1, "ao")
-mat_plasma.set_density("g/cm3", 7)
 
+materials = openmc.Materials([mat_layer_1, mat_layer_2, mat_layer_3, mat_layer_4, mat_layer_5])
 
-materials = openmc.Materials([mat_layer_1, mat_layer_2, mat_layer_3, mat_layer_4, mat_layer_5, mat_plasma])
-
+wedge_angles = (0, 180)
 
 
 dag_univ = openmc.DAGMCUniverse(filename=h5m_filename)
-bound_dag_univ = dag_univ.bounded_universe()
-geometry = openmc.Geometry(root=bound_dag_univ)
+# bound_dag_univ = dag_univ.bounded_universe() wedge not currently supported, PR open
+
+bbox = dag_univ.bounding_box
+
+radius_upper_right = np.linalg.norm(
+    np.array(bbox[0][0], bbox[0][1]) - np.array([0.0, 0.0])
+)
+radius_lower_left = np.linalg.norm(
+    np.array(bbox[1][0], bbox[1][1]) - np.array([0.0, 0.0])
+)
+radius = max(radius_upper_right, radius_lower_left)
+print('bbox',bbox)
+print('radius', radius)
+print('radius_upper_right', radius_upper_right)
+print('radius_lower_left', radius_lower_left)
+
+cylinder_surface = openmc.ZCylinder(r=radius, boundary_type='vacuum', surface_id=1000)
+
+wedge_angle_surf_1 = openmc.Plane(
+    a=math.sin(math.radians(wedge_angles[0])),
+    b=-math.cos(math.radians(wedge_angles[0])),
+    c=0.0,
+    d=0.0,
+    boundary_type='reflective',
+    surface_id=1001
+)
+
+wedge_angle_surf_2 = openmc.Plane(
+    a=math.sin(math.radians(wedge_angles[1])),
+    b=-math.cos(math.radians(wedge_angles[1])),
+    c=0.0,
+    d=0.0,
+    boundary_type='reflective',
+    surface_id=1002
+)
+
+lower_z = openmc.ZPlane(bbox[0][2], boundary_type='vacuum', surface_id=1003)
+upper_z = openmc.ZPlane(bbox[1][2], boundary_type='vacuum', surface_id=1004)
+
+if wedge_angles[1] - wedge_angles[0] >= 180.0:
+    region = (
+        -cylinder_surface
+        & +lower_z
+        & -upper_z
+        & (-wedge_angle_surf_1 | +wedge_angle_surf_2)
+    )
+else:
+    region = (
+        -cylinder_surface
+        & +lower_z
+        & -upper_z
+        & -wedge_angle_surf_1
+        & +wedge_angle_surf_2
+    )
+
+bounding_cell = openmc.Cell(
+    fill=dag_univ,
+    cell_id=10000,
+    region=region
+)
+# universe = openmc.Universe(cells=[bounding_cell])
+
+
+# geometry = openmc.Geometry([bounding_cell])
+# import matplotlib.pyplot as plt
+# geometry.plot()
+# plt.show
 
 # initializes a new source object
 my_source = openmc.IndependentSource()
@@ -81,13 +148,13 @@ my_source = openmc.IndependentSource()
 my_source = openmc.IndependentSource()
 
 # the distribution of radius is just a single value
-radius = openmc.stats.Discrete([10], [1])
+radius = openmc.stats.Discrete([my_reactor.major_radius], [1])
 
 # the distribution of source z values is just a single value
 z_values = openmc.stats.Discrete([0], [1])
 
 # the distribution of source azimuthal angles values is a uniform distribution between 0 and 2 Pi
-angle = openmc.stats.Uniform(a=0., b=2* 3.14159265359)
+angle = openmc.stats.Uniform(a=0., b=np.pi)
 
 # this makes the ring source using the three distributions and a radius
 my_source.space = openmc.stats.CylindricalIndependent(r=radius, phi=angle, z=z_values, origin=(0.0, 0.0, 0.0))
@@ -108,11 +175,11 @@ settings.source = my_source
 
 # adds a tally to record the heat deposited in a mesh overlaid over entire geometry
 
-mesh = openmc.RegularMesh().from_domain(bound_dag_univ)
+mesh = openmc.RegularMesh().from_domain(geometry)
 mesh.dimension=[100, 100, 100]
-mesh_tally = openmc.Tally(name="flux")
+mesh_tally = openmc.Tally(name="heating")
 mesh_tally.filters=[openmc.MeshFilter(mesh)]
-mesh_tally.scores = ["flux"]
+mesh_tally.scores = ["heating"]
 tallies = openmc.Tallies([mesh_tally])
 
 # # builds the openmc model
@@ -124,9 +191,11 @@ output_file = my_model.run()
 # # loads up the output file from the simulation
 statepoint = openmc.StatePoint(output_file)
 
-my_flux_mesh_tally = statepoint.get_tally(name="flux")
+my_heating_mesh_tally = statepoint.get_tally(name="heating")
 
 mesh.write_data_to_vtk(
-    datasets={'mean': my_flux_mesh_tally.mean.flatten()},
-    filename='mesh_tally_results.vtk',
+    datasets={'mean': my_heating_mesh_tally.mean.flatten()},
+    filename='structured_mesh_tally_results.vtk',
 )
+
+print('VTK file saved to structured_mesh_tally_results.vtk')
