3cf6c738c60f9bce42640b78956c2baa439383f2
shimwell <mail@jshimwell.com>
2024-12-28 22:39:18 +0100

    bioshiled added

--- examples/tokamak_structured_mesh_simulation.py
+++ examples/tokamak_structured_mesh_simulation.py
@@ -1,151 +1,149 @@
+"""
+This script sets up and runs a neutronics simulation for a tokamak reactor using OpenMC and Paramak.
+It performs the following steps:
+1. Defines the tokamak reactor geometry and materials.
+2. Creates a DAGMC model from the CAD geometry.
+3. Sets up the simulation environment and source.
+4. Runs the simulation and records the heating tally.
+5. Outputs the results to a VTK file.
+"""
+from pathlib import Path
 import openmc
 import numpy as np
 import math
 import paramak
 import cadquery as cq
+from cad_to_dagmc import CadToDagmc
 
-openmc.config['cross_sections'] = '/nuclear_data/cross_sections.xml' # change this to the dir of your cross_sections.xml
+# change this path to the cross_sections.xml to the path of your cross_sections.xml
+openmc.config["cross_sections"] = "/nuclear_data/cross_sections.xml"
 
+
+
+bioshield_to_reactor_gap = 500
+bioshield_radial_thickness = 200
+floor_vertical_thickness = 200
+ceiling_vertical_thickness = 200
+reactor_to_floor_gap = 100
+reactor_to_ceiling_gap = 150
 my_reactor = paramak.tokamak_from_plasma(
     radial_build=[
         (paramak.LayerType.GAP, 10),
-        (paramak.LayerType.SOLID, 30), # layer_1
-        (paramak.LayerType.SOLID, 50), # layer_2
-        (paramak.LayerType.SOLID, 10), # layer_3
-        (paramak.LayerType.SOLID, 120), # layer_4
-        (paramak.LayerType.SOLID, 20), # layer_5
+        (paramak.LayerType.SOLID, 30),  # layer_1
+        (paramak.LayerType.SOLID, 50),  # layer_2
+        (paramak.LayerType.SOLID, 10),  # layer_3
+        (paramak.LayerType.SOLID, 120),  # layer_4
+        (paramak.LayerType.SOLID, 20),  # layer_5
         (paramak.LayerType.GAP, 60),
-        (paramak.LayerType.PLASMA, 300), # plasma
+        (paramak.LayerType.PLASMA, 300),  # plasma
         (paramak.LayerType.GAP, 60),
-        (paramak.LayerType.SOLID, 20), # layer_3
-        (paramak.LayerType.SOLID, 120), # layer_4
-        (paramak.LayerType.SOLID, 10), # layer_5
+        (paramak.LayerType.SOLID, 20),  # layer_3
+        (paramak.LayerType.SOLID, 120),  # layer_4
+        (paramak.LayerType.SOLID, 10),  # layer_5
     ],
     elongation=2,
     triangularity=0.55,
     rotation_angle=180,
 )
-my_reactor=my_reactor.remove(name='plasma')
 
-# Translate the my_assembly object so that its bottom is at z=0
-translation_vector = cq.Vector(0, 0, -my_reactor.BoundingBox().zmin)
-my_assembly = my_assembly.translate(translation_vector)
-
-print('translation_vector', translation_vector)
+# removing the plasma from the CadQuery Assembly as we don't need this in the 
+# DAGMC mesh for the neutronics simulation.
+my_reactor = my_reactor.remove(name="plasma")
 
 
-# this prints all the names of the parts in the reactor
+# this prints all the names of the parts in the reactor, this is useful for
+# knowing the material tags to use in the simulation
 print(my_reactor.names())
 
-from cad_to_dagmc import CadToDagmc
-my_model = CadToDagmc()
 material_tags = ["layer_1", "layer_2", "layer_3", "layer_4", "layer_5"]
+my_model = CadToDagmc()
 
-# 6 material tags as inner and outer layers are one solid there are only 6 solids in model
+# 6 material tags as inner and outer layers are one solid there are only 6 solids in model and plasma has been removed
 my_model.add_cadquery_object(cadquery_object=my_reactor, material_tags=material_tags)
 
-from pathlib import Path
-script_path = Path(__file__).resolve()
-script_folder = script_path.parent
-h5m_filename =  script_folder/"dagmc.h5m"
+script_folder = Path(__file__).resolve().parent
+h5m_filename = script_folder / "dagmc.h5m"
 # my_model.export_dagmc_h5m_file(filename=h5m_filename, min_mesh_size=10.0, max_mesh_size=20.0)
 
 # simplified material definitions have been used to keen this example minimal
-mat_layer_1 = openmc.Material(name='layer_1')
-mat_layer_1.add_element('Cu', 1, "ao")
+mat_layer_1 = openmc.Material(name="layer_1")
+mat_layer_1.add_element("Cu", 1, "ao")
 mat_layer_1.set_density("g/cm3", 7)
 
-mat_layer_2 = openmc.Material(name='layer_2')
-mat_layer_2.add_nuclide('W186', 1, "ao")
+mat_layer_2 = openmc.Material(name="layer_2")
+mat_layer_2.add_nuclide("W186", 1, "ao")
 mat_layer_2.set_density("g/cm3", 0.01)
 
-mat_layer_3 = openmc.Material(name='layer_3')
-mat_layer_3.add_nuclide('Fe56', 1, "ao")
+mat_layer_3 = openmc.Material(name="layer_3")
+mat_layer_3.add_nuclide("Fe56", 1, "ao")
 mat_layer_3.set_density("g/cm3", 7)
 
-mat_layer_4 = openmc.Material(name='layer_4')
-mat_layer_4.add_element('Li', 1, "ao")
+mat_layer_4 = openmc.Material(name="layer_4")
+mat_layer_4.add_element("Li", 1, "ao")
 mat_layer_4.set_density("g/cm3", 0.5)
 
-mat_layer_5 = openmc.Material(name='layer_5')
-mat_layer_5.add_nuclide('Fe56', 1, "ao")
+mat_layer_5 = openmc.Material(name="layer_5")
+mat_layer_5.add_nuclide("Fe56", 1, "ao")
 mat_layer_5.set_density("g/cm3", 7)
 
-mat_concrete = openmc.Material(name='concrete')
-mat_concrete.add_nuclide('Fe56', 1, "ao")
+mat_concrete = openmc.Material(name="concrete")
+mat_concrete.add_nuclide("Fe56", 1, "ao")
 mat_concrete.set_density("g/cm3", 7)
 
 
 materials = openmc.Materials([mat_layer_1, mat_layer_2, mat_layer_3, mat_layer_4, mat_layer_5, mat_concrete])
 
-wedge_angles = (0, 180)
-
 dag_univ = openmc.DAGMCUniverse(filename=h5m_filename)
 
-bioshield_to_reactor_gap = 500
-bioshield_radial_thickness = 200
-z_padding = 400
-
 bbox = dag_univ.bounding_box
+dagmc_geometry_offset = abs(bbox[0][2]) + floor_vertical_thickness + reactor_to_floor_gap
 
 dagmc_radius = max(abs(bbox[0][0]), abs(bbox[0][1]), abs(bbox[1][0]), abs(bbox[1][1]))
 
-bioshield_inner_surface = openmc.ZCylinder(r=dagmc_radius+bioshield_to_reactor_gap, surface_id=1000)
-
-wedge_angle_surf_1 = openmc.Plane(
-    a=math.sin(math.radians(wedge_angles[0])),
-    b=-math.cos(math.radians(wedge_angles[0])),
-    c=0.0,
-    d=0.0,
-    boundary_type='reflective',
-    surface_id=1001
+side_surface = openmc.YPlane(y0=0, boundary_type="reflective", surface_id=1001)
+bioshield_inner_surface = openmc.ZCylinder(r=dagmc_radius + bioshield_to_reactor_gap, surface_id=1002)
+# floor_upper_surface = openmc.ZPlane(z0=floor_vertical_thickness + reactor_to_floor_gap, surface_id=1002)
+ceiling_upper_surface = openmc.ZPlane(
+    z0=floor_vertical_thickness
+    + reactor_to_floor_gap
+    + reactor_to_ceiling_gap
+    + bbox.width[2]
+    + ceiling_vertical_thickness,
+    boundary_type="vacuum",
+    surface_id=1003,
+)
+ceiling_lower_surface = openmc.ZPlane(
+    z0=floor_vertical_thickness + reactor_to_floor_gap + reactor_to_ceiling_gap + bbox.width[2], surface_id=1004
+)
+floor_upper_surface = openmc.ZPlane(z0=floor_vertical_thickness, surface_id=1005)
+floor_lower_surface = openmc.ZPlane(z0=0, boundary_type="vacuum", surface_id=1006)
+bioshield_outer_surface = openmc.ZCylinder(
+    r=dagmc_radius + bioshield_to_reactor_gap + bioshield_radial_thickness, boundary_type="vacuum", surface_id=1007
 )
 
-wedge_angle_surf_2 = openmc.Plane(
-    a=math.sin(math.radians(wedge_angles[1])),
-    b=-math.cos(math.radians(wedge_angles[1])),
-    c=0.0,
-    d=0.0,
-    boundary_type='reflective',
-    surface_id=1002
-)
 
-lower_z = openmc.ZPlane(0, surface_id=1003)
-upper_z = openmc.ZPlane(abs(bbox[0][2])+bbox[1][2], surface_id=1004)
-# lower_z = openmc.ZPlane(bbox[0][2], boundary_type='vacuum', surface_id=1003)
-# upper_z = openmc.ZPlane(bbox[1][2], boundary_type='vacuum', surface_id=1004)
+wedge_region = -bioshield_inner_surface & +floor_upper_surface & -ceiling_lower_surface & +side_surface
 
 
-wedge_region = (
-        -bioshield_inner_surface
-        & +lower_z
-        & -upper_z
-        & (-wedge_angle_surf_1 | +wedge_angle_surf_2)
-    )
-
-
-bounding_cell = openmc.Cell(
-    fill=dag_univ,
-    cell_id=1000,
-    region=wedge_region
-)
+bounding_cell = openmc.Cell(fill=dag_univ, cell_id=1000, region=wedge_region)
 # moving the geometry so that the bottom of is at z=0
 # to do this we need to move the geometry up as it is centered around z=0
 # we also need to move it up so the the start of the earth layer is at z=0
-dagmc_geometry_offset = abs(bbox[0][2])
-bounding_cell.translation = (0,0,dagmc_geometry_offset)
+bounding_cell.translation = (0, 0, dagmc_geometry_offset)
 
 
-floor_lower_surface = openmc.ZPlane(z0=0, boundary_type='vacuum', surface_id=1005)
-bioshield_outer_surface = openmc.ZCylinder(r=dagmc_radius+bioshield_to_reactor_gap, boundary_type='vacuum', surface_id=1006)
-
-bioshield_region = -bioshield_outer_surface & +bioshield_inner_surface & +lower_z & -upper_z & (-wedge_angle_surf_1 | +wedge_angle_surf_2)
+bioshield_region = (
+    -bioshield_outer_surface & +bioshield_inner_surface & +floor_upper_surface & -ceiling_lower_surface & +side_surface
+)
 bioshield_cell = openmc.Cell(region=bioshield_region, fill=mat_concrete, cell_id=1001)
 
-floor_region = -lower_z & +floor_lower_surface & -bioshield_outer_surface & (-wedge_angle_surf_1 | +wedge_angle_surf_2)
+floor_region = -floor_upper_surface & +floor_lower_surface & -bioshield_outer_surface & +side_surface
 floor_cell = openmc.Cell(region=floor_region, fill=mat_concrete, cell_id=1002)
 
-geometry = openmc.Geometry([bounding_cell, floor_cell, bioshield_cell])
+ceiling_region = -ceiling_upper_surface & +ceiling_lower_surface & -bioshield_outer_surface & +side_surface
+ceiling_cell = openmc.Cell(region=ceiling_region, fill=mat_concrete, cell_id=1003)
+
+geometry = openmc.Geometry([bounding_cell, floor_cell, bioshield_cell, ceiling_cell])
 
 # import matplotlib.pyplot as plt
 # geometry.plot()
@@ -161,10 +159,10 @@ my_source = openmc.IndependentSource()
 radius = openmc.stats.Discrete([my_reactor.major_radius], [1])
 
 # the distribution of source z values is just a single value
-z_values = openmc.stats.Discrete([0], [1])
+z_values = openmc.stats.Discrete([dagmc_geometry_offset], [1])
 
 # the distribution of source azimuthal angles values is a uniform distribution between 0 and 2 Pi
-angle = openmc.stats.Uniform(a=0., b=np.pi)
+angle = openmc.stats.Uniform(a=0.0, b=np.pi)
 
 # this makes the ring source using the three distributions and a radius
 my_source.space = openmc.stats.CylindricalIndependent(r=radius, phi=angle, z=z_values, origin=(0.0, 0.0, 0.0))
@@ -185,10 +183,16 @@ settings.source = my_source
 
 # adds a tally to record the heat deposited in a mesh overlaid over entire geometry
 
-mesh = openmc.RegularMesh().from_domain(geometry)
-mesh.dimension=[100, 100, 100]
+mesh = openmc.CylindricalMesh(
+    r_grid=np.linspace(0, dagmc_radius + bioshield_to_reactor_gap + bioshield_radial_thickness, 100),
+    z_grid=np.linspace(0, floor_vertical_thickness+ reactor_to_floor_gap+ reactor_to_ceiling_gap+ bbox.width[2] + ceiling_vertical_thickness, 100),
+    origin=(0, 0, 0),
+    phi_grid=np.linspace(0, np.pi, 100),
+)
+    # phi_grid
+    #  dimension = [100, 100, 100])
 mesh_tally = openmc.Tally(name="heating")
-mesh_tally.filters=[openmc.MeshFilter(mesh)]
+mesh_tally.filters = [openmc.MeshFilter(mesh)]
 mesh_tally.scores = ["heating"]
 tallies = openmc.Tallies([mesh_tally])
 
@@ -204,8 +208,11 @@ statepoint = openmc.StatePoint(output_file)
 my_heating_mesh_tally = statepoint.get_tally(name="heating")
 
 mesh.write_data_to_vtk(
-    datasets={'mean': my_heating_mesh_tally.mean.flatten()},
-    filename='structured_mesh_tally_results.vtk',
+    curvilinear=True,
+    datasets={"mean": my_heating_mesh_tally.mean.flatten()},
+    filename="structured_mesh_tally_results.vtk",
 )
 
-print('VTK file saved to structured_mesh_tally_results.vtk')
+print("VTK file saved to structured_mesh_tally_results.vtk")
+
+
